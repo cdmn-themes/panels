6d789f2af7fa85ce15284cf6bc9d5fbca1b630ae
Stefan Buhrmester <buhrmi@gmail.com>
2022-11-08 15:40:19 +0100

    bla

--- src/lib/reservation.svelte
+++ src/lib/reservation.svelte
@@ -15,10 +15,11 @@
 
 <div  class="fixed top-0 z-10 w-full h-full bg-dark/60 flex items-center justify-center" on:click|self>
   <div transition:fly={{y: 20}} class="bg-white/50 p-2 text-center">
-    <div class="bg-white p-8 text-center">
+    <div class="bg-white p-8 md:px-30 text-center md:text-lg">
       <h2>Ihre Reservierung</h2>
       <p class="mb-2">Telefonische Reservierung unter <b><a href="tel:+4922894556556">0228 94 556 556</a></b></p>
-      <p>Gerne können Sie auch online eine Reservierung durchführen.<br>Nutzen Sie dazu das folgende Formular.</p>
+      <p>Gerne nehmen wir Ihre Reservierung online entgegen.<br>Nutzen Sie dazu das folgende Formular.</p>
+      <hr class="mt-4">
     <div bind:this={form}></div>
   </div>
   
@@ -29,5 +30,9 @@
 <style>
   :global(iframe) {
     display: inline-block;
+  }
+  hr {
+    border-top: 1px solid #ddd;
+    width: 100%;
   }
 </style>
