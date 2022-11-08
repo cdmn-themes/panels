7146535602b7d85a3882161612e79d121dc59687
Stefan Buhrmester <buhrmi@gmail.com>
2022-11-08 15:13:14 +0100

    reserve

--- src/lib/reservation.svelte
+++ src/lib/reservation.svelte
@@ -1,7 +1,7 @@
 <script>
   import { page } from '$app/stores';
   import { onMount } from 'svelte';
-  import { fade } from 'svelte/transition';
+  import { fly } from 'svelte/transition';
   let form
   onMount(() => {
     // inject opentable script
@@ -13,8 +13,14 @@
 </script>
 
 
-<div transition:fade class="fixed top-0 w-full h-full bg-dark/60 flex items-center justify-center" on:click|self>
+<div  class="fixed top-0 z-10 w-full h-full bg-dark/60 flex items-center justify-center" on:click|self>
+  <div transition:fly={{y: 20}} class="bg-white p-8">
+    <h2>Lassen Sie sich verwöhnen</h2>
+    <p class="mb-2">Telefonische Reservierung unter <b><a href="tel:+4922894556556">0228 94 556 556</a></b></p>
+    <p>Gerne können Sie auch online eine Reservierung durchführen.<br>Nutzen Sie dazu das folgende Formular.</p>
   <div bind:this={form}></div>
+  
+</div>
 
 </div>
 
